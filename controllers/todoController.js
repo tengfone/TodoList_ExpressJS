45278643c2a460c8dda1cb10f525e7af84a0928c
Teng Fone <phangtf@yahoo.com.sg>
2020-01-09 16:54:57 +0800

    Finalized

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -2,9 +2,8 @@
 const bodyParser = require('body-parser');
 
 // Connecting to MongoDB Atlas
-var mongoose = require('mongoose');
-
-// Connect here
+const mongoose = require('mongoose');
+mongoose.connect('mongodb+srv://<USERNAME>:<PASSWORD>@nodejstodolist-h800z.mongodb.net/test?retryWrites=true&w=majority', { useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true }).catch(error => handleError(error));
 
 // Schema
 var todoSchema = new mongoose.Schema({
@@ -12,33 +11,43 @@ var todoSchema = new mongoose.Schema({
 });
 
 var Todo = mongoose.model('Todo',todoSchema);
-var itemOne = Todo({item:'Click To Delete an item!'}).save(function(err){
-   if(err) throw err;
-   console.log('Item Saved');
-});
 
-let data = [{item:'Click To Delete an item!'}];
+
+// LOCAL DATA: let data = [{item:'Click To Delete an item!'}];
 const urlencodedParser = bodyParser.urlencoded({extended:false});
 
 module.exports = function(app){
 
     app.get('/todo',function(req,res){
-        res.render('todo',{todos:data});
+        // get data from mongoDB and pass to view
+        // find everything
+        Todo.find({}, function(err,data){
+            if (err) throw err;
+            res.render('todo',{todos:data});
+        });
     });
 
     app.post('/todo',urlencodedParser,function(req,res){
-        data.push(req.body);
-
-        // RELOAD THE ENTIRE SCRIPT
-        res.json(data);
+        // get data from the view and upload to MongoDB
+        const newTodo = Todo(req.body).save(function(err,data){
+            if (err) throw err;
+            // reload the data on view
+            res.json(data);
+        });
     });
 
     app.delete('/todo/:item',function(req,res){
-        data = data.filter(function(todo){
-            return todo.item.replace(/ /g, '-') !== req.params.item;
+        // delete the requested item from MongoDB
+        Todo.find({item: req.params.item.replace(/\-/g, " ")}).remove(function(err,data){
+            if (err) throw err;
+            res.json(data);
         });
-
-        // RELOAD THE ENTIRE SCRIPT
-        res.json(data);
+        // LOCAL
+        // data = data.filter(function(t0do){
+        //     return t0do.item.replace(/ /g, '-') !== req.params.item;
+        // });
+        //
+        // // RELOAD THE ENTIRE SCRIPT
+        // res.json(data);
     });
 };
